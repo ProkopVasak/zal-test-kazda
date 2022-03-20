a3c6b28147b6b755e00fdbb0b4542ff8006e2986
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2022-03-20 11:17:46 +0100

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,64 +1,50 @@
 pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
-up = False
-down = False
-right = False
-left = False
 x = 2
 y = 2
-
+def on_forever():
+    led.plot(x, y) 
+forever(on_forever)
 def on_button_pressed_a():
-    global left
+    global x
     if x <= 0:
         pass
     else:
-        left = True
-        basic.pause(20)
-        left = False
+        basic.clear_screen()
+        x -= 1
+        led.plot(x, y)
+        
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
 def on_button_pressed_b():
-    global right
+    global x
     if x >= 4:
         pass
     else:
-        right = True
-        basic.pause(20)
-        right = False
+        basic.clear_screen()
+        x += 1
+        led.plot(x, y)
 input.on_button_pressed(Button.B, on_button_pressed_b)
-
 def on_pin_pressed_p2():
-    global down
+    global y
     if y >= 4:
         pass
     else:
-        down = True
-        basic.pause(20)
-        down = False
+        basic.clear_screen()
+        y += 1
+        led.plot(x, y)
 input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
-
+def on_button_pressed_ab():
+    global x, y
+    x = 2
+    y = 2
+    basic.clear_screen()
+    led.plot(x, y)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 def on_logo_event_pressed():
-    global up
+    global y
     if y <= 0:
         pass
     else:
-        up = True
-        basic.pause(20)
-        up = False
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
-
-def start():
-    global up, down, right, left, x, y
-    led.plot(x, y)
-    if up == True:
         basic.clear_screen()
         y -= 1
-    if down == True:
-        basic.clear_screen()
-        y += 1
-    if right == True:
-        basic.clear_screen()
-        x += 1
-    if left == True:
-        basic.clear_screen()
-        x -= 1
-forever(start)
+        led.plot(x, y)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
-let up = false
-let down = false
-let right = false
-let left = false
 let x = 2
 let y = 2
+forever(function on_forever() {
+    led.plot(x, y)
+})
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     if (x <= 0) {
         
     } else {
-        left = true
-        basic.pause(20)
-        left = false
+        basic.clearScreen()
+        x -= 1
+        led.plot(x, y)
     }
     
 })
@@ -21,9 +20,9 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     if (x >= 4) {
         
     } else {
-        right = true
-        basic.pause(20)
-        right = false
+        basic.clearScreen()
+        x += 1
+        led.plot(x, y)
     }
     
 })
@@ -32,44 +31,27 @@ input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
     if (y >= 4) {
         
     } else {
-        down = true
-        basic.pause(20)
-        down = false
+        basic.clearScreen()
+        y += 1
+        led.plot(x, y)
     }
     
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    x = 2
+    y = 2
+    basic.clearScreen()
+    led.plot(x, y)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
     
     if (y <= 0) {
         
     } else {
-        up = true
-        basic.pause(20)
-        up = false
-    }
-    
-})
-forever(function start() {
-    
-    led.plot(x, y)
-    if (up == true) {
         basic.clearScreen()
         y -= 1
-    }
-    
-    if (down == true) {
-        basic.clearScreen()
-        y += 1
-    }
-    
-    if (right == true) {
-        basic.clearScreen()
-        x += 1
-    }
-    
-    if (left == true) {
-        basic.clearScreen()
-        x -= 1
+        led.plot(x, y)
     }
     
 })
